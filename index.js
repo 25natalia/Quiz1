f19b2612f48ff540bbcc57488c59fff192fd75a0
25natalia <25natalia09garcia@gmail.com>
2024-02-09 17:49:31 -0500

    quiz

---
+++ index.js
@@ -0,0 +1,22 @@
+const grades = [
+	{
+		score: 'A',
+	},
+	{
+		score: 'B',
+	},
+	{
+		score: 'C',
+	},
+	{
+		score: 'D',
+	},
+	{
+		score: 'F',
+	},
+];
+
+const studentsGrades = words.filter((grade) => grade > 60);
+
+const studentsGardes = [100, 5, 65, 70, 85, 96, 45];
+const result = console.log(result);
